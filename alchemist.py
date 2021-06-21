cc8cbff7a4921b94b3f2cb8d10f6167d85deea9e
Tommy Keswick <tkeswick@caltech.edu>
2021-06-21 16:59:58 -0700

    run processes based on init filename

--- alchemist.py
+++ alchemist.py
@@ -4,9 +4,10 @@
 # TODO notification of errors from this file
 
 import os
+import shutil
+import subprocess
 from datetime import datetime
 from glob import glob
-from subprocess import run
 
 from decouple import config
 
@@ -16,10 +17,61 @@ for f in glob(os.path.join(config("PROCESSING_FILES"), "*-init-*")):
     PYTHON_CMD = config("PYTHON_CMD")
     print(f"📅 {datetime.now()}")
     print(f"🗄 {collection_id}")
-    run(
-        [
-            PYTHON_CMD,
-            os.path.join(os.path.dirname(os.path.abspath(__file__)), "distill.py"),
-            collection_id,
-        ]
-    )
+    if os.path.basename(f).split("-")[-1] == "report":
+        pass
+    elif os.path.basename(f).split("-")[-1] == "preservation":
+        # delete the init file
+        os.remove(f)
+        subprocess.run(
+            [
+                PYTHON_CMD,
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), "distill.py"),
+                collection_id,
+            ]
+        )
+    elif os.path.basename(f).split("-")[-1] == "access":
+        # delete the init file
+        os.remove(f)
+        # move the `collection_id` directory into `STAGE_2_ORIGINAL_FILES`
+        # NOTE shutil.move() in Python < 3.9 needs strings as arguments
+        try:
+            shutil.move(
+                str(os.path.join(config("STAGE_1_ORIGINAL_FILES"), collection_id)),
+                str(config("STAGE_2_ORIGINAL_FILES")),
+            )
+        except Exception as e:
+            # TODO log a problem and notify
+            raise
+        subprocess.run(
+            [
+                PYTHON_CMD,
+                os.path.join(
+                    os.path.dirname(os.path.abspath(__file__)),
+                    f"{config('ACCESS_PLATFORM')}.py",
+                ),
+                collection_id,
+            ]
+        )
+    elif os.path.basename(f).split("-")[-1] == "preservation_access":
+        # delete the init file
+        os.remove(f)
+        subprocess.run(
+            [
+                PYTHON_CMD,
+                os.path.join(os.path.dirname(os.path.abspath(__file__)), "distill.py"),
+                collection_id,
+            ]
+        )
+        subprocess.run(
+            [
+                PYTHON_CMD,
+                os.path.join(
+                    os.path.dirname(os.path.abspath(__file__)),
+                    f"{config('ACCESS_PLATFORM')}.py",
+                ),
+                collection_id,
+            ]
+        )
+    else:
+        # TODO log a message that an unknown file was found
+        pass
