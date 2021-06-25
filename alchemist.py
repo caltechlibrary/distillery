06da5893ea80482ed321a517a6ee1af4ed978eb4
Tommy Keswick <tkeswick@caltech.edu>
2021-06-25 14:30:50 -0700

    catch exceptions better

--- alchemist.py
+++ alchemist.py
@@ -35,13 +35,21 @@ for f in glob(os.path.join(config("PROCESSING_FILES"), "*-init-*")):
         logger.info("⚗️ preservation processing")
         # delete the init file
         os.remove(f)
-        subprocess.run(
-            [
-                PYTHON_CMD,
-                os.path.join(os.path.dirname(os.path.abspath(__file__)), "distill.py"),
-                collection_id,
-            ]
-        )
+        try:
+            subprocess.run(
+                [
+                    PYTHON_CMD,
+                    os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), "distill.py"
+                    ),
+                    collection_id,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                check=True,
+            )
+        except BaseException as e:
+            logger.error(f"❌ {e.stdout.decode('utf-8')}")
     elif os.path.basename(f).split("-")[-1] == "access":
         logger.info("⚗️ access processing")
         # delete the init file
@@ -49,52 +57,81 @@ for f in glob(os.path.join(config("PROCESSING_FILES"), "*-init-*")):
         # move the `collection_id` directory into `STAGE_2_ORIGINAL_FILES`
         # NOTE shutil.move() in Python < 3.9 needs strings as arguments
         try:
-            shutil.move(
-                # TODO need to rethink file locations; some files may be explicitly not
-                # sent to islandora, so islandora.py should not look for files in the
-                # same place; probably need two different locations: one for when
-                # originals are not to be published and one for when they may be
+            # TODO need to rethink file locations; some files may be explicitly not
+            # sent to islandora, so islandora.py should not look for files in the
+            # same place; probably need two different locations: one for when
+            # originals are not to be published and one for when they may be
+            shutil.copytree(
                 str(os.path.join(config("STAGE_1_ORIGINAL_FILES"), collection_id)),
                 str(config("STAGE_2_ORIGINAL_FILES")),
+                dirs_exist_ok=True
             )
-        except Exception as e:
-            # TODO log a problem and notify
-            logger.error(
-                f"❌ unable to move directory: {str(os.path.join(config('STAGE_1_ORIGINAL_FILES'), collection_id))}"
+            shutil.rmtree(
+                str(os.path.join(config("STAGE_1_ORIGINAL_FILES"), collection_id))
             )
-            logger.error(e)
+            # shutil.move(
+            #     str(os.path.join(config("STAGE_1_ORIGINAL_FILES"), collection_id)),
+            #     str(config("STAGE_2_ORIGINAL_FILES")),
+            # )
+        # except FileNotFoundError as e:
+        #     # NOTE FileNotFoundError is not caught with BaseException
+        #     logger.error(f"❌ {e}")
+        #     # we re-raise the exception because we cannot continue without the files
+        #     raise
+        except BaseException as e:
+            logger.error(f"❌ {e}")
             raise
-        subprocess.run(
-            [
-                PYTHON_CMD,
-                os.path.join(
-                    os.path.dirname(os.path.abspath(__file__)),
-                    f"{config('ACCESS_PLATFORM')}.py",
-                ),
-                collection_id,
-            ]
-        )
+        try:
+            subprocess.run(
+                [
+                    PYTHON_CMD,
+                    os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)),
+                        f"{config('ACCESS_PLATFORM')}.py",
+                    ),
+                    collection_id,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                check=True,
+            )
+        except BaseException as e:
+            logger.error(f"❌ {e.stdout.decode('utf-8')}")
     elif os.path.basename(f).split("-")[-1] == "preservation_access":
         logger.info("⚗️ preservation & access processing")
         # delete the init file
         os.remove(f)
-        subprocess.run(
-            [
-                PYTHON_CMD,
-                os.path.join(os.path.dirname(os.path.abspath(__file__)), "distill.py"),
-                collection_id,
-            ]
-        )
-        subprocess.run(
-            [
-                PYTHON_CMD,
-                os.path.join(
-                    os.path.dirname(os.path.abspath(__file__)),
-                    f"{config('ACCESS_PLATFORM')}.py",
-                ),
-                collection_id,
-            ]
-        )
+        try:
+            subprocess.run(
+                [
+                    PYTHON_CMD,
+                    os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)), "distill.py"
+                    ),
+                    collection_id,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                check=True,
+            )
+        except BaseException as e:
+            logger.error(f"❌ {e.stdout.decode('utf-8')}")
+        try:
+            subprocess.run(
+                [
+                    PYTHON_CMD,
+                    os.path.join(
+                        os.path.dirname(os.path.abspath(__file__)),
+                        f"{config('ACCESS_PLATFORM')}.py",
+                    ),
+                    collection_id,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+                check=True,
+            )
+        except BaseException as e:
+            logger.error(f"❌ {e.stdout.decode('utf-8')}")
     else:
         # TODO log a message that an unknown file was found
         pass
