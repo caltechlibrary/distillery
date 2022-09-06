c8e68761182d1c85cf305f4772fb582065a693ca
Tommy Keswick <t4k@users.noreply.github.com>
2022-09-06 11:05:49 -0700

    add a missing comma

--- oralhistories.py
+++ oralhistories.py
@@ -100,7 +100,7 @@ def convert_word_to_markdown(docxfile, transcript_dir):
     pandoc_cmd(
         "--standalone",
         "--table-of-contents",
-        f'--metadata-file={transcript_dir.joinpath("metadata.json")}'
+        f'--metadata-file={transcript_dir.joinpath("metadata.json")}',
         f'--output={transcript_dir.joinpath(f"{transcript_dir.stem}.md")}',
         docxfile,
     )
