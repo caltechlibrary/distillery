fd9a21427ee5abbe5cd2dc10d376fa1ec9012c32
Tommy Keswick <t4k@users.noreply.github.com>
2022-09-28 14:07:19 -0700

    set up regenerate script; use token

--- oralhistories/generate.py
+++ oralhistories/generate.py
@@ -17,49 +17,53 @@ def main(file):
             os.remove(f"{os.path.splitext(file)[0]}.html")
             os.remove(f"{os.path.splitext(file)[0]}.pdf")
             return
-        os.makedirs(f"build/{file_segments[1]}", exist_ok=True)
-        # generate html
-        subprocess.run(
-            [
-                "pandoc",
-                "--standalone",
-                "--shift-heading-level-by=1",
-                "--table-of-contents",
-                "--from=markdown",
-                "--to=html",
-                "--template=.github/workflows/templates/web.html",
-                f"--output=build/{file_segments[1]}/{file_segments[1]}.html",
-                f"transcripts/{file_segments[1]}/{file_segments[1]}.md",
-            ]
-        )
-        print(f"🐞 file generated: build/{file_segments[1]}/{file_segments[1]}.html")
-        # create intermediate html for pagedjs
-        subprocess.run(
-            [
-                "pandoc",
-                "--standalone",
-                "--shift-heading-level-by=1",
-                "--table-of-contents",
-                "--from=markdown",
-                "--to=html",
-                "--template=.github/workflows/templates/pdf.html",
-                f"--output=build/{file_segments[1]}/tmp.html",
-                f"transcripts/{file_segments[1]}/{file_segments[1]}.md",
-            ]
-        )
-        print(f"🐞 file generated: build/{file_segments[1]}/tmp.html")
-        # create pdf with pagedjs
-        subprocess.run(
-            [
-                "pagedjs-cli",
-                f"build/{file_segments[1]}/tmp.html",
-                "--output",
-                f"transcripts/{file_segments[1]}/{file_segments[1]}.pdf",
-            ]
-        )
-        print(f"🐞 file generated: build/{file_segments[1]}/{file_segments[1]}.pdf")
-        os.remove(f"build/{file_segments[1]}/tmp.html")
-        print(f"🐞 file deleted: build/{file_segments[1]}/tmp.html")
+        generate_files(file_segments[1])
+
+
+def generate_files(identifier):
+    os.makedirs(f"build/{identifier}", exist_ok=True)
+    # generate html
+    subprocess.run(
+        [
+            "pandoc",
+            "--standalone",
+            "--shift-heading-level-by=1",
+            "--table-of-contents",
+            "--from=markdown",
+            "--to=html",
+            "--template=.github/workflows/templates/web.html",
+            f"--output=build/{identifier}/{identifier}.html",
+            f"transcripts/{identifier}/{identifier}.md",
+        ]
+    )
+    print(f"🐞 file generated: build/{identifier}/{identifier}.html")
+    # create intermediate html for pagedjs
+    subprocess.run(
+        [
+            "pandoc",
+            "--standalone",
+            "--shift-heading-level-by=1",
+            "--table-of-contents",
+            "--from=markdown",
+            "--to=html",
+            "--template=.github/workflows/templates/pdf.html",
+            f"--output=build/{identifier}/tmp.html",
+            f"transcripts/{identifier}/{identifier}.md",
+        ]
+    )
+    print(f"🐞 file generated: build/{identifier}/tmp.html")
+    # create pdf with pagedjs
+    subprocess.run(
+        [
+            "pagedjs-cli",
+            f"build/{identifier}/tmp.html",
+            "--output",
+            f"transcripts/{identifier}/{identifier}.pdf",
+        ]
+    )
+    print(f"🐞 file generated: build/{identifier}/{identifier}.pdf")
+    os.remove(f"build/{identifier}/tmp.html")
+    print(f"🐞 file deleted: build/{identifier}/tmp.html")
 
 
 if __name__ == "__main__":
--- oralhistories/regenerate.py
+++ oralhistories/regenerate.py
@@ -1,8 +1,14 @@
+import os
 import sys
+
+import generate
 
 
 def main(identifier):
     print(f"🐞 identifier: {identifier}")
+    if os.path.isfile(f"transcripts/{identifier}/{identifier}.md"):
+        generate.generate_files(identifier)
+
 
 if __name__ == "__main__":
     main(sys.argv[1])
