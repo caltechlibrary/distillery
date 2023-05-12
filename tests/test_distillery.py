c7410c3a5328e2694fff63b1a4c1a7f110cc3496
Tommy Keswick <t4k@users.noreply.github.com>
2023-05-11 18:10:37 -0700

    ✅ add setup & testing for s3 objects

--- tests/test_distillery.py
+++ tests/test_distillery.py
@@ -3,9 +3,7 @@ import glob
 import os
 import pytest
 import shutil
-import subprocess
 
-from asnake.client import ASnakeClient
 from decouple import config
 from playwright.sync_api import Page, expect
 
@@ -34,21 +32,41 @@ def distillery_0000_reset_files():
     return
 
 
+@pytest.fixture
+def asnake_client():
+    from asnake.client import ASnakeClient
+
+    asnake_client = ASnakeClient(
+        baseurl=config("ASPACE_API_URL"),
+        username=config("ASPACE_USERNAME"),
+        password=config("ASPACE_PASSWORD"),
+    )
+    asnake_client.authorize()
+    return asnake_client
+
+
+@pytest.fixture
+def s3_client():
+    import boto3
+
+    s3_client = boto3.client(
+        "s3",
+        region_name=config("DISTILLERY_AWS_REGION", default="us-west-2"),
+        aws_access_key_id=config("DISTILLERY_AWS_ACCESS_KEY_ID"),
+        aws_secret_access_key=config("DISTILLERY_AWS_SECRET_ACCESS_KEY"),
+    )
+    return s3_client
+
+
 # NOTE cleaning up previous tests and then setting up for new tests is not the
 # recommended way to do Arrange and Cleanup in pytest, but it allows manually
 # viewing the outcome of tests in ArchivesSpace
 
 
-def test_distillery_0001_setup_wrong_component_id_948vk(page: Page):
+def test_distillery_0001_setup_wrong_component_id_948vk(page: Page, asnake_client):
     """Corresponding directory name does not match component_id."""
     # NOTE without page parameter test does not run in order
     try:
-        asnake_client = ASnakeClient(
-            baseurl=config("ASPACE_API_URL"),
-            username=config("ASPACE_USERNAME"),
-            password=config("ASPACE_PASSWORD"),
-        )
-        asnake_client.authorize()
         # DELETE ANY EXISTING TEST RECORDS
         resource_948vk_find_by_id_results = asnake_client.get(
             "/repositories/2/find_by_id/resources",
@@ -101,96 +119,89 @@ def test_distillery_0001_setup_wrong_component_id_948vk(page: Page):
         raise
 
 
-def test_distillery_0001_setup_nonnumeric_sequence_gz36p(page: Page):
+def test_distillery_0001_setup_nonnumeric_sequence_gz36p(
+    page: Page, asnake_client, s3_client
+):
     """Sequence strings on TIFF files are alphanumeric not numeric."""
-    # NOTE without page parameter test does not run in order
-    try:
-        asnake_client = ASnakeClient(
-            baseurl=config("ASPACE_API_URL"),
-            username=config("ASPACE_USERNAME"),
-            password=config("ASPACE_PASSWORD"),
-        )
-        asnake_client.authorize()
-        # DELETE ANY EXISTING TEST RECORDS
-        resource_find_by_id_results = asnake_client.get(
-            "/repositories/2/find_by_id/resources",
-            params={"identifier[]": ['["DistilleryTEST-gz36p"]']},
+    # NOTE without page parameter test does not seem to run in order
+    # DELETE ANY EXISTING TEST RECORDS
+    resource_find_by_id_results = asnake_client.get(
+        "/repositories/2/find_by_id/resources",
+        params={"identifier[]": ['["DistilleryTEST-gz36p"]']},
+    ).json()
+    for resource in resource_find_by_id_results["resources"]:
+        resource_tree = asnake_client.get(f'{resource["ref"]}/tree/root').json()
+        item_uri = resource_tree["precomputed_waypoints"][""]["0"][0]["uri"]
+        archival_object = asnake_client.get(
+            item_uri, params={"resolve[]": "digital_object"}
         ).json()
-        for resource in resource_find_by_id_results["resources"]:
-            resource_tree = asnake_client.get(f'{resource["ref"]}/tree/root').json()
-            item_uri = resource_tree["precomputed_waypoints"][""]["0"][0]["uri"]
-            archival_object = asnake_client.get(
-                item_uri, params={"resolve[]": "digital_object"}
-            ).json()
-            for instance in archival_object["instances"]:
-                if instance.get("digital_object"):
-                    digital_object_delete_response = asnake_client.delete(
-                        instance["digital_object"]["_resolved"]["uri"]
-                    )
-            resource_delete_response = asnake_client.delete(resource["ref"])
-        # CREATE RESOURCE RECORD
-        resource = {}
-        # required
-        resource["title"] = "_DISTILLERY TEST RESOURCE gz36p"
-        resource["id_0"] = "DistilleryTEST-gz36p"
-        resource["level"] = "collection"
-        resource["finding_aid_language"] = "eng"
-        resource["finding_aid_script"] = "Latn"
-        resource["lang_materials"] = [
-            {"language_and_script": {"language": "eng", "script": "Latn"}}
-        ]
-        resource["dates"] = [
-            {
-                "label": "creation",
-                "date_type": "single",
-                "begin": str(datetime.date.today()),
-            }
-        ]
-        resource["extents"] = [
-            {"portion": "whole", "number": "1", "extent_type": "boxes"}
-        ]
-        # post
-        resource_post_response = asnake_client.post(
-            "/repositories/2/resources", json=resource
-        )
-        print(
-            "🐞 resource_post_response:DistilleryTEST-gz36p",
-            resource_post_response.json(),
-        )
-        # CREATE ITEM RECORD
-        item = {}
-        # required
-        item["title"] = "_DISTILLERY TEST ITEM gz36p"
-        item["level"] = "item"
-        item["resource"] = {"ref": resource_post_response.json()["uri"]}
-        # optional
-        item["component_id"] = "item-gz36p"
-        # post
-        item_post_response = asnake_client.post(
-            "/repositories/2/archival_objects", json=item
-        )
-    except Exception:
-        raise
+        for instance in archival_object["instances"]:
+            if instance.get("digital_object"):
+                digital_object_delete_response = asnake_client.delete(
+                    instance["digital_object"]["_resolved"]["uri"]
+                )
+        resource_delete_response = asnake_client.delete(resource["ref"])
+    # CREATE RESOURCE RECORD
+    resource = {}
+    # required
+    resource["title"] = "_DISTILLERY TEST RESOURCE gz36p"
+    resource["id_0"] = "DistilleryTEST-gz36p"
+    resource["level"] = "collection"
+    resource["finding_aid_language"] = "eng"
+    resource["finding_aid_script"] = "Latn"
+    resource["lang_materials"] = [
+        {"language_and_script": {"language": "eng", "script": "Latn"}}
+    ]
+    resource["dates"] = [
+        {
+            "label": "creation",
+            "date_type": "single",
+            "begin": str(datetime.date.today()),
+        }
+    ]
+    resource["extents"] = [{"portion": "whole", "number": "1", "extent_type": "boxes"}]
+    # post
+    resource_post_response = asnake_client.post(
+        "/repositories/2/resources", json=resource
+    )
+    print(
+        "🐞 resource_post_response:DistilleryTEST-gz36p",
+        resource_post_response.json(),
+    )
+    # CREATE ITEM RECORD
+    item = {}
+    # required
+    item["title"] = "_DISTILLERY TEST ITEM gz36p"
+    item["level"] = "item"
+    item["resource"] = {"ref": resource_post_response.json()["uri"]}
+    # optional
+    item["component_id"] = "item-gz36p"
+    # post
+    item_post_response = asnake_client.post(
+        "/repositories/2/archival_objects", json=item
+    )
+    # DELETE S3 OBJECTS
+    s3_response = s3_client.list_objects_v2(
+        Bucket=config("PRESERVATION_BUCKET"), Prefix="DistilleryTEST-gz36p"
+    )
+    print("🐞 s3_client.list_objects_v2", s3_response)
+    s3_keys = [{"Key": s3_object["Key"]} for s3_object in s3_response["Contents"]]
+    s3_response = s3_client.delete_objects(
+        Bucket=config("PRESERVATION_BUCKET"), Delete={"Objects": s3_keys}
+    )
 
 
-def test_distillery_0001_setup(page: Page):
+def test_distillery_0001_setup(page: Page, asnake_client):
     # NOTE without page parameter test does not run in order
 
     try:
-        asnake_client = ASnakeClient(
-            baseurl=config("ASPACE_API_URL"),
-            username=config("ASPACE_USERNAME"),
-            password=config("ASPACE_PASSWORD"),
-        )
-        asnake_client.authorize()
-
         # DELETE ANY EXISTING TEST RECORDS
+        # delete digital objects
         resource_0001_find_by_id_results = asnake_client.get(
             "/repositories/2/find_by_id/resources",
             params={"identifier[]": ['["DistilleryTEST0001_collection"]']},
         ).json()
         print("🐞 resource_0001_find_by_id_results", resource_0001_find_by_id_results)
-        # delete digital objects
         for resource in resource_0001_find_by_id_results["resources"]:
             resource_0001_tree = asnake_client.get(
                 f'{resource["ref"]}/tree/root'
@@ -362,7 +373,9 @@ def test_distillery_cloud_wrong_component_id_948vk(page: Page):
     )
 
 
-def test_distillery_cloud_nonnumeric_sequence_gz36p(page: Page):
+def test_distillery_cloud_nonnumeric_sequence_gz36p(
+    page: Page, asnake_client, s3_client
+):
     page.goto(config("BASE_URL"))
     page.get_by_label("Collection ID").fill("DistilleryTEST-gz36p")
     page.get_by_text(
@@ -378,13 +391,12 @@ def test_distillery_cloud_nonnumeric_sequence_gz36p(page: Page):
     expect(page.locator("p")).to_have_text(
         "✅ Processed metadata and files for DistilleryTEST-gz36p.", timeout=60000
     )
-    # ensure that the digital object components were created correctly
-    asnake_client = ASnakeClient(
-        baseurl=config("ASPACE_API_URL"),
-        username=config("ASPACE_USERNAME"),
-        password=config("ASPACE_PASSWORD"),
+    # get a list of s3 objects under this test prefix
+    s3_response = s3_client.list_objects_v2(
+        Bucket=config("PRESERVATION_BUCKET"), Prefix="DistilleryTEST-gz36p"
     )
-    asnake_client.authorize()
+    print("🐞 s3_client.list_objects_v2", s3_response)
+    # ensure that the digital object components were created correctly
     results = asnake_client.get(
         "/repositories/2/find_by_id/digital_objects",
         params={"digital_object_id[]": "item-gz36p"},
@@ -395,33 +407,12 @@ def test_distillery_cloud_nonnumeric_sequence_gz36p(page: Page):
         tree = asnake_client.get(f'{digital_object["ref"]}/tree/root').json()
         print(f'🐞 {digital_object["ref"]}/tree/root', tree)
         assert len(tree["precomputed_waypoints"][""]["0"]) > 0
-        assert (
-            tree["precomputed_waypoints"][""]["0"][0]["label"]
-            == "DistilleryTEST-gz36p-item_C.tiff"
-        )
-        assert tree["precomputed_waypoints"][""]["0"][0]["file_uri_summary"].startswith(
-            f's3://{config("PRESERVATION_BUCKET")}/DistilleryTEST-gz36p/item-gz36p--DISTILLERY-TEST-ITEM-gz36p/DistilleryTEST-gz36p-item_C.tiff/'
-        )
-        assert tree["precomputed_waypoints"][""]["0"][0]["file_uri_summary"].endswith(
-            ".jp2"
-        )
-        assert (
-            tree["precomputed_waypoints"][""]["0"][1]["label"]
-            == "DistilleryTEST-gz36p-item_p001.tiff"
-        )
-        assert tree["precomputed_waypoints"][""]["0"][1]["file_uri_summary"].startswith(
-            f's3://{config("PRESERVATION_BUCKET")}/DistilleryTEST-gz36p/item-gz36p--DISTILLERY-TEST-ITEM-gz36p/DistilleryTEST-gz36p-item_p001.tiff/'
-        )
-        assert tree["precomputed_waypoints"][""]["0"][1]["file_uri_summary"].endswith(
-            ".jp2"
-        )
-        assert (
-            tree["precomputed_waypoints"][""]["0"][2]["label"]
-            == "DistilleryTEST-gz36p-item_p002.tiff"
-        )
-        assert tree["precomputed_waypoints"][""]["0"][2]["file_uri_summary"].startswith(
-            f's3://{config("PRESERVATION_BUCKET")}/DistilleryTEST-gz36p/item-gz36p--DISTILLERY-TEST-ITEM-gz36p/DistilleryTEST-gz36p-item_p002.tiff/'
-        )
-        assert tree["precomputed_waypoints"][""]["0"][2]["file_uri_summary"].endswith(
-            ".jp2"
-        )
+        for waypoint in tree["precomputed_waypoints"][""]["0"]:
+            # split the s3 key from the file_uri_summary and ensure it matches
+            assert waypoint["file_uri_summary"].split(
+                f's3://{config("PRESERVATION_BUCKET")}/'
+            )[-1] in [s3_object["Key"] for s3_object in s3_response["Contents"]]
+            # split the original filename from the s3 key and match the label
+            assert waypoint["label"] in [
+                s3_object["Key"].split("/")[-2] for s3_object in s3_response["Contents"]
+            ]
