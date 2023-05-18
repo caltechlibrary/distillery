440f8f531f369dc072b8fcc341f6f385f5a6b3f2
Tommy Keswick <t4k@users.noreply.github.com>
2023-05-17 17:28:01 -0700

    ✅ set a timeout; consolidate setup with test

--- tests/test_distillery.py
+++ tests/test_distillery.py
@@ -63,62 +63,6 @@ def s3_client():
 # viewing the outcome of tests in ArchivesSpace
 
 
-def test_distillery_0001_setup_wrong_component_id_948vk(page: Page, asnake_client):
-    """Corresponding directory name does not match component_id."""
-    # NOTE without page parameter test does not run in order
-    try:
-        # DELETE ANY EXISTING TEST RECORDS
-        resource_948vk_find_by_id_results = asnake_client.get(
-            "/repositories/2/find_by_id/resources",
-            params={"identifier[]": ['["DistilleryTEST-948vk"]']},
-        ).json()
-        print("🐞 resource_948vk_find_by_id_results", resource_948vk_find_by_id_results)
-        for resource in resource_948vk_find_by_id_results["resources"]:
-            resource_948vk_delete_response = asnake_client.delete(resource["ref"])
-            print(
-                "🐞 resource_948vk_delete_response",
-                resource_948vk_delete_response.json(),
-            )
-        # CREATE RESOURCE RECORD
-        resource_948vk = {}
-        resource_948vk["title"] = "_DISTILLERY TEST RESOURCE 948vk"  # required
-        resource_948vk["id_0"] = "DistilleryTEST-948vk"  # required
-        resource_948vk["level"] = "collection"  # required
-        resource_948vk["finding_aid_language"] = "eng"  # required
-        resource_948vk["finding_aid_script"] = "Latn"  # required
-        resource_948vk["lang_materials"] = [
-            {"language_and_script": {"language": "eng", "script": "Latn"}}
-        ]  # required
-        resource_948vk["dates"] = [
-            {
-                "label": "creation",
-                "date_type": "single",
-                "begin": str(datetime.date.today()),
-            }
-        ]  # required
-        resource_948vk["extents"] = [
-            {"portion": "whole", "number": "1", "extent_type": "boxes"}
-        ]  # required
-        resource_948vk_post_response = asnake_client.post(
-            "/repositories/2/resources", json=resource_948vk
-        )
-        print("🐞 resource_948vk_post_response", resource_948vk_post_response.json())
-        # CREATE ITEM RECORD
-        item_948vk = {}
-        item_948vk["title"] = "_DISTILLERY TEST ITEM 948vk"  # title or date required
-        item_948vk["component_id"] = "item_948vk"
-        item_948vk["level"] = "item"  # required
-        item_948vk["resource"] = {
-            "ref": resource_948vk_post_response.json()["uri"]
-        }  # required
-        item_948vk_post_response = asnake_client.post(
-            "/repositories/2/archival_objects", json=item_948vk
-        )
-        print("🐞 item_948vk_post_response", item_948vk_post_response.json())
-    except Exception:
-        raise
-
-
 def test_distillery_0001_setup_nonnumeric_sequence_gz36p(
     page: Page, asnake_client, s3_client
 ):
@@ -351,7 +295,51 @@ def test_distillery_landing(page: Page):
     expect(page).to_have_title("Distillery")
 
 
-def test_distillery_cloud_wrong_component_id_948vk(page: Page):
+def test_distillery_cloud_wrong_component_id_948vk(page: Page, asnake_client):
+    """Corresponding directory name does not match component_id."""
+    # DELETE ANY EXISTING TEST RECORDS
+    delete_archivesspace_test_records(asnake_client, "DistilleryTEST-948vk")
+    # CREATE COLLECTION RECORD
+    resource = {}
+    # NOTE required
+    resource["title"] = "_DISTILLERY TEST COLLECTION 948vk"
+    resource["id_0"] = "DistilleryTEST-948vk"
+    resource["level"] = "collection"
+    resource["finding_aid_language"] = "eng"
+    resource["finding_aid_script"] = "Latn"
+    resource["lang_materials"] = [
+        {"language_and_script": {"language": "eng", "script": "Latn"}}
+    ]
+    resource["dates"] = [
+        {
+            "label": "creation",
+            "date_type": "single",
+            "begin": str(datetime.date.today()),
+        }
+    ]
+    resource["extents"] = [{"portion": "whole", "number": "1", "extent_type": "boxes"}]
+    # NOTE post
+    resource_post_response = asnake_client.post(
+        "/repositories/2/resources", json=resource
+    )
+    print(
+        "🐞 resource_post_response:DistilleryTEST-948vk",
+        resource_post_response.json(),
+    )
+    # CREATE ITEM RECORD
+    item = {}
+    # NOTE required
+    item["title"] = "_DISTILLERY TEST ITEM 948vk"
+    item["level"] = "item"
+    item["resource"] = {"ref": resource_post_response.json()["uri"]}
+    # NOTE optional
+    item["component_id"] = "distillery_test_item_948vk"
+    # NOTE post
+    item_post_response = asnake_client.post(
+        "/repositories/2/archival_objects", json=item
+    )
+    print("🐞 item_post_response:948vk", item_post_response.json())
+    # DISTILLERY CLOUD WORKFLOW
     page.goto(config("DISTILLERY_BASE_URL"))
     page.get_by_label("Collection ID").fill("DistilleryTEST-948vk")
     page.get_by_text(
@@ -360,7 +348,7 @@ def test_distillery_cloud_wrong_component_id_948vk(page: Page):
     page.get_by_role("button", name="Validate").click()
     page.get_by_text("Details").click()
     expect(page.locator("p")).to_have_text(
-        "❌ Something went wrong. View the details for more information."
+        "❌ Something went wrong. View the details for more information.", timeout=20000
     )
 
 
